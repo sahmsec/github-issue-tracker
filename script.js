b7646f0373fcadc29cf13867287c6acfac5e4c77
Sakib Ahmed <sakibahmedsadhin@gmail.com>
2026-03-09 07:10:06 +0600

    issue container

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ console.log("js connected!");
 
 const loginBtn = document.getElementById("loginBtn");
 
-loginBtn.addEventListener("click", function(){
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+if (loginBtn) {
 
-    if(username === "admin" && password === "admin123"){
-        window.location.href = "main.html";
-    }
-    else{
-        alert("Invalid Credentials!");
-    }
-})
+    loginBtn.addEventListener("click", function () {
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (username === "admin" && password === "admin123") {
+            window.location.href = "main.html";
+        } else {
+            alert("Invalid Credentials!");
+        }
+
+    });
+
+    // Enter key login
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            loginBtn.click();
+        }
+    });
+
+}
